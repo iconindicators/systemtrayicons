0d33bb5552f073d04b9b818d90690dc7a27aa478
Bernard Giannetti <thebernmeister@hotmail.com>
2007-06-22 12:58:48 +0000

    Now sets the system Look and Feel.

--- src/StardateSystemTray.java
+++ src/StardateSystemTray.java
@@ -1,11 +1,20 @@
 import java.awt.SystemTray;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 
 public class StardateSystemTray
 {
     public StardateSystemTray()
 	{
-    	if( ! SystemTray.isSupported() )
+        try { UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() ); }
+        catch( IllegalAccessException illegalAccessException ) {  }
+        catch( InstantiationException instantiationException ) {  }
+        catch( ClassNotFoundException classNotFoundException ) {  }
+        catch( UnsupportedLookAndFeelException unsupportedLookAndFeelException ) {  }
+
+        if( ! SystemTray.isSupported() )
 	    	return;
 
     	SystemTray systemTray = SystemTray.getSystemTray();
