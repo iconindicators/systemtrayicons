e960babc42c4f20ea2f2454628e0a94b80734d52
Bernard Giannetti <thebernmeister@hotmail.com>
2007-07-24 01:08:03 +0000

    Added i18n.

--- src/StardateSystemTray.java
+++ src/StardateSystemTray.java
@@ -1,5 +1,6 @@
 import java.awt.SystemTray;
 
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -15,20 +16,29 @@ public class StardateSystemTray
         catch( UnsupportedLookAndFeelException unsupportedLookAndFeelException ) {  }
 
         if( ! SystemTray.isSupported() )
-	    	return;
+        {
+			JOptionPane.showMessageDialog
+			(
+				null, 
+				Messages.getString( "StardateSystemTray.0" ),  
+				Messages.getString( "StardateSystemTray.1" ),  
+				JOptionPane.ERROR_MESSAGE
+			);
 
-    	SystemTray systemTray = SystemTray.getSystemTray();
-	 
+			return;
+        }
+
     	TrayIcon trayIcon = TrayIcon.createTrayIcon( new PopupMenu() );
+    	SystemTray systemTray = SystemTray.getSystemTray();
 		try { systemTray.add( trayIcon ); }
-		catch( Throwable throwable ) { }
-
+		catch( Throwable throwable ) 
+		{
+			JOptionPane.showMessageDialog( null, throwable.getMessage(), Messages.getString( "StardateSystemTray.2" ), JOptionPane.ERROR_MESSAGE );
+			return;
+		}
 		trayIcon.displayStartupBalloon();
 	}
 
 
-    public static void main( String[] args )
-	{
-        new StardateSystemTray();
-	}
+    public static void main( String[] args ) { new StardateSystemTray(); }
 }
