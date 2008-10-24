a32a253ef6de1534144e5e775cf23b18250a1d85
Bernard Giannetti <thebernmeister@hotmail.com>
2008-10-24 12:34:25 +0000

    Update for latest Java.

--- antBuild.cmd
+++ antBuild.cmd
@@ -3,7 +3,7 @@ cls
 setlocal
 
 rem ===== Setup environment for ant.
-set ANT_HOME=C:/progra~1/apache-ant-1.7.0
+set ANT_HOME=C:/progra~1/apache-ant-1.7.1
 set JAVA_HOME=C:/Progra~1/Java/jdk1.6.0_07
 set NSIS_HOME=C:/Progra~1/NSIS
 set PATH=%PATH%;%ANT_HOME%\bin;
